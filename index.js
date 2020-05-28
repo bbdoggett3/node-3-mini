7f04547bdfeaeefe516c5b288fc7d08d82fbfa77
bbdoggett3 <bbdoggett3@gmail.com>
2020-05-27 19:41:26 -0600

    create database table

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+require('dotenv').config();
 const express = require("express");
+const massive = require('massive')
 
 const app = express();
 
@@ -6,6 +8,10 @@ const { SERVER_PORT } = process.env;
 
 app.use(express.json());
 
+//endpoints here
+
 app.listen(SERVER_PORT, () => {
   console.log(`Server listening on port ${SERVER_PORT}`);
 });
+
+
