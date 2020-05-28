bd45e7e380b25d4e1a367b531878adbf025896f0
bbdoggett3 <bbdoggett3@gmail.com>
2020-05-27 19:59:24 -0600

    add in a index.js file

--- index.js
+++ index.js
@@ -4,14 +4,21 @@ const massive = require('massive')
 
 const app = express();
 
-const { SERVER_PORT } = process.env;
+const { SERVER_PORT, CONNECTION_STRING } = process.env;
+
+massive(  {
+  connectionString: CONNECTION_STRING,
+  ssl: {
+    rejectUnauthorized: false
+  }
+}).then(db => app.set('db', db)); 
 
 app.use(express.json());
 
 //endpoints here
 
+
+
 app.listen(SERVER_PORT, () => {
   console.log(`Server listening on port ${SERVER_PORT}`);
 });
-
-
